402d727672fd6fc1303c78fb90a09217833d7358
Jeikob7 <evgeniybikov7@gmail.com>
2015-11-13 02:07:35 +0300

    asd

--- 123.java
+++ 123.java
@@ -1,5 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("NE MOGU OSTANOVITSYA!");
+        EMAE.jon();  
+    }
+}
+class EMAE {
+    public static void jon(String[] args) {
+        System.out.println("asldasldk");
     }
 }
