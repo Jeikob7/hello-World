2570b622da52205e85ab6727546be6414e95f48a
Jeikob7 <evgeniybikov7@gmail.com>
2015-11-13 02:28:14 +0300

    df

--- 123.java
+++ 123.java
@@ -1,11 +1,11 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("NE MOGU OSTANOVITSYA!");
-        EMAE.jon();  
+        EMAE.jon("asldasldk");  
     }
 }
 class EMAE {
-    public static void jon(String[] args) {
+    public static void jon(String args) {
         System.out.println("asldasldk");
     }
 }
